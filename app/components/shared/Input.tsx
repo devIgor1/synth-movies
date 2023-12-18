4d0261753db4bcd4dfaaad33c78119150c08d4e4
Igor <igormraesmm@gmail.com>
2023-12-18 13:28:08 -0300

    feat: Add input as a component

--- app/components/shared/Input.tsx
+++ app/components/shared/Input.tsx
@@ -1,19 +1,34 @@
 "use client"
 
+import { RegisterOptions, UseFormRegister } from "react-hook-form"
+
 interface InputProps {
-  type: string | any
-  placeholder: string | any
-  className: string
+  type: string
+  placeholder?: string
+  name: string
+  register: UseFormRegister<any>
+  error?: string
+  rules?: RegisterOptions
 }
 
-export function Input({ placeholder, type }: InputProps) {
+export function Input({
+  name,
+  placeholder,
+  register,
+  type,
+  rules,
+  error,
+}: InputProps) {
   return (
     <>
       <input
+        className="outline-none bg-transparent text-white caret-vhs"
         placeholder={placeholder}
         type={type}
-        className="outline-none bg-transparent text-white"
+        {...register(name, rules)}
+        id={name}
       />
+      {error && <p className="text-red-500 my-1">{error}</p>}
     </>
   )
 }
