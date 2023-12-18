2dfbfcb9cc394803d1a9e822015c80040cb6f859
Igor <igormraesmm@gmail.com>
2023-12-18 14:51:05 -0300

    build: Credentials configuration

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,22 @@
-import { CredentialsProvider } from "next-auth/providers/credentials"
+import CredentialsProvider from "next-auth/providers/credentials"
+import { NextAuthOptions } from "next-auth"
 import NextAuth from "next-auth/next"
-import bcrypt from "bcrypt"
+
+const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+          const response = await 
+      },
+    }),
+  ],
+}
+
+const handler = NextAuth(authOptions)
+
+export { handler as GET, handler as POST }
