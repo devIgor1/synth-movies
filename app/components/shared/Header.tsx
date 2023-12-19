b444b78f16f34bedcc5e01ef57cd46d6085bc274
Igor <igormraesmm@gmail.com>
2023-12-19 13:21:28 -0300

    feat: Add useSession to verify users

--- app/components/shared/Header.tsx
+++ app/components/shared/Header.tsx
@@ -1,7 +1,11 @@
+"use client"
+import { signOut, useSession } from "next-auth/react"
 import Image from "next/image"
 import Link from "next/link"
 
 const Header = () => {
+  const { status, data } = useSession()
+
   return (
     <div className="w-full font-vhs">
       <nav className="md:mx-14 p-5 flex-between">
@@ -18,21 +22,37 @@ const Header = () => {
             className=""
           />
         </Link>
-        <div className="text-white text-2xl md:text-3xl">
-          <Link
-            href="/sign-in"
-            className="hover:text-pinkNeon hover:animate-pulse hover:bg-white"
-          >
-            Login
-          </Link>
-          <span className="px-2 animate-pulse">|</span>
-          <Link
-            href="/sign-up"
-            className="hover:text-pinkNeon hover:animate-pulse hover:bg-white"
-          >
-            Register
-          </Link>
-        </div>
+        {status === "unauthenticated" && (
+          <div className="text-white text-2xl md:text-3xl">
+            <Link
+              href="/sign-in"
+              className="hover:text-pinkNeon hover:animate-pulse hover:bg-white"
+            >
+              Login
+            </Link>
+            <span className="px-2 animate-pulse">|</span>
+            <Link
+              href="/sign-up"
+              className="hover:text-pinkNeon hover:animate-pulse hover:bg-white"
+            >
+              Register
+            </Link>
+          </div>
+        )}
+
+        {status === "authenticated" && (
+          <div className="text-white text-2xl md:text-3xl">
+            <h1 className="hover:text-pinkNeon hover:animate-pulse hover:bg-white">
+              {data.user?.name}
+            </h1>
+            <button
+              onClick={() => signOut()}
+              className="hover:text-pinkNeon hover:animate-pulse hover:bg-white"
+            >
+              Logout
+            </button>
+          </div>
+        )}
       </nav>
       <div className="border-b-2 border-pinkNeon"></div>
     </div>
