e2604700d16239364cbdbf0a7c5d892bf62978b0
Igor <igormraesmm@gmail.com>
2023-12-20 11:05:48 -0300

    feat: Protect signin and signup pages

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,18 @@
+import { authOptions } from "../api/auth/[...nextauth]/route"
+import { getServerSession } from "next-auth"
+import { redirect } from "next/navigation"
+import { ReactNode } from "react"
+
+interface PrivateLayoutProps {
+  children: ReactNode
+}
+
+export default async function PrivateLayout({ children }: PrivateLayoutProps) {
+  const session = await getServerSession(authOptions)
+
+  if (session) {
+    redirect("/")
+  }
+
+  return <>{children}</>
+}
