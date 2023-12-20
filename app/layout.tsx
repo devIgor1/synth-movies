f9d03ee8e580387c90fe18672d564f4fd90bd38b
Igor <igormraesmm@gmail.com>
2023-12-20 12:20:44 -0300

    style: Remove poppins

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,6 @@
 import type { Metadata } from "next"
-import { Poppins } from "next/font/google"
 import "./globals.css"
 import AuthProvider from "./components/providers/auth-provider"
-
-const poppins = Poppins({
-  subsets: ["latin"],
-  weight: ["400", "500", "600", "700"],
-  variable: "--font-poppins",
-})
 
 export const metadata: Metadata = {
   title: "Synth Movies",
@@ -24,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={poppins.className}>
+      <body>
         <AuthProvider>{children}</AuthProvider>
       </body>
     </html>
