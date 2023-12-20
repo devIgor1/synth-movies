4ef58555746da5b23bee3c45f1149deb989d1e5d
Igor <igormraesmm@gmail.com>
2023-12-20 16:16:54 -0300

    feat: Finish homepage

--- app/(root)/home/page.tsx
+++ app/(root)/home/page.tsx
@@ -1,9 +1,12 @@
 import Header from "@/app/components/shared/Header"
+import Image from "next/image"
 import { IoIosSearch } from "react-icons/io"
+import { FaHeart } from "react-icons/fa"
+import Link from "next/link"
 
 export default function Home() {
   return (
-    <div className="w-full h-full bg-background-image bg-no-repeat bg-center">
+    <div className="w-full min-h-screen bg-background-image bg-no-repeat bg-center">
       <Header />
       <div className="wrapper flex-center font-vhs">
         <div className="relative w-full max-w-[320px] md:max-w-[460px]">
@@ -17,6 +20,33 @@ export default function Home() {
             <span className="cursor-pointer animate-pulse hover:scale-110 duration-300">
               <IoIosSearch size={34} />
             </span>
+          </section>
+        </div>
+      </div>
+      <div className="wrapper">
+        <div className=" bg-[#040110] font-vhs rounded-lg w-full h-full">
+          <h1 className="text-3xl text-white pinkNeon-text-shadow p-4">
+            Latest Releases
+          </h1>
+          <div className="border-b-2 border-blueNeon  w-full"></div>
+          <section className="w-full grid grid-cols-1 md:gap-6 md:grid-cols-2 lg:grid-cols-4 p-5 rounded-lg">
+            <section className="w-full rounded-lg border-pinkNeon border-2 p-2 ">
+              <Link href="/movie/:id">
+                <img
+                  className="w-full h-full max-h-[300px]"
+                  src="/assets/images/movieTheme.jpg"
+                  alt="movie"
+                />
+              </Link>
+              <div className="flex items-center justify-between py-2 mb-5">
+                <h1 className="text-white font-bold pinkNeon-text-shadow">
+                  NE.AS
+                </h1>
+                <span className="text-pinkNeon">
+                  <FaHeart />
+                </span>
+              </div>
+            </section>
           </section>
         </div>
       </div>
