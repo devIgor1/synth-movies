b2983b15408d45503542bdc4ce294d3fe09626e2
Igor <igormraesmm@gmail.com>
2023-12-23 09:53:43 -0300

    style: Initial style for my-movies page

--- app/(root)/dashboard/my-movies/page.tsx
+++ app/(root)/dashboard/my-movies/page.tsx
@@ -1,8 +1,8 @@
 const MyMovies = () => {
   return (
-    <div>
+    <main className="w-full h-screen bg-my-movies-bg bg-center bg-cover bg-no-repeat">
       <h1>My Movies</h1>
-    </div>
+    </main>
   )
 }
 
