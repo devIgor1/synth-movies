50d6ebd2bdfa5340e5ad1b25d3dfa9e67e9a5558
Igor <igormraesmm@gmail.com>
2023-12-24 12:31:19 -0300

    refactor: Remove edit button

--- app/(root)/dashboard/my-movies/page.tsx
+++ app/(root)/dashboard/my-movies/page.tsx
@@ -1,12 +1,12 @@
 import Link from "next/link"
 import { RiDeleteBinLine } from "react-icons/ri"
-import { GrEdit } from "react-icons/gr"
 import { PiPopcornBold } from "react-icons/pi"
 import { LuCupSoda } from "react-icons/lu"
 import { IoCaretBackOutline } from "react-icons/io5"
 import prisma from "@/lib/db"
 import { getServerSession } from "next-auth"
 import { authOptions } from "@/lib/auth"
+import Image from "next/image"
 
 const MyMovies = async () => {
   const session = await getServerSession(authOptions)
@@ -21,56 +21,74 @@ const MyMovies = async () => {
     <main className="w-full min-h-screen bg-my-movies bg-center bg-cover bg-no-repeat font-vhs">
       <Link
         href="/dashboard"
-        className="text-white flex items-center text-3xl hover:underline p-2 mb-32"
+        className="text-white flex items-center text-3xl hover:underline p-2 mb-10"
       >
         <IoCaretBackOutline size={30} color="#EB139A" />
         Back to Dashboard
       </Link>
       <div className="wrapper">
         <div className="flex-center p-3 w-full flex-col">
-          <div className="bg-darkPurple rounded-lg p-14 md:p-10 shadow-2xl shadow-pinkNeon  ">
-            <div className="border-double border-4 border-blueNeon shadow-lg shadow-blueNeon rounded-lg relative">
-              <div className="flex-center text-[#FCFCBB] bg-darkPurple gap-3 p-0 md:pb-2 absolute -top-9 left-6 md:-left-4">
-                <span className="-rotate-12">
-                  <LuCupSoda size={50} />
-                </span>
-                <h1 className="text-2xl md:text-5xl lg:text-5xl blue-text-shadow">
-                  My Movies
-                </h1>
-                <span className="rotate-12">
-                  <PiPopcornBold size={50} />
-                </span>
-              </div>
-              <section className="w-full grid g grid-cols-1 md:grid-cols-2 lg:grid-cols-4 p-10 gap-6 rounded-lg">
-                {movies.map((movie) => (
-                  <section
-                    className="w-full rounded-lg border-pinkNeon shadow-lg shadow-pinkNeon border-2"
-                    key={movie.id}
+          <div className="bg-darkPurple rounded-lg p-14 md:p-10 shadow-2xl shadow-pinkNeon">
+            {movies.length === 0 ? (
+              <div className="flex-center flex-col">
+                <h1 className="text-3xl text-white pb-4">
+                  Produce your film{" "}
+                  <Link
+                    href="/dashboard/movie/new"
+                    className="text-blueNeon underline"
                   >
-                    <Link href="/movie/:id">
-                      <img
-                        className="w-full rounded-lg"
-                        src={movie.cover}
-                        alt="movie"
-                      />
-                    </Link>
-                    <div className="flex items-center justify-between py-3 border-t-2 border-pinkNeon px-2">
-                      <h1 className="text-[#FDC580] font-bold">
-                        {movie.title}
-                      </h1>
-                      <div className="flex-center gap-3">
-                        <span className="text-white">
-                          <RiDeleteBinLine size={22} />
-                        </span>
-                        <span className="text-white">
-                          <GrEdit size={22} />
-                        </span>
+                    HERE
+                  </Link>
+                  !
+                </h1>
+                <Image
+                  src="/assets/images/nomovie.jpeg"
+                  alt="no-movie-error"
+                  width={600}
+                  height={500}
+                />
+              </div>
+            ) : (
+              <div className="border-double border-4 border-blueNeon shadow-lg shadow-blueNeon rounded-lg relative">
+                <div className="flex-center text-[#FCFCBB] bg-darkPurple gap-3 p-0 md:pb-2 absolute -top-9 left-6 md:-left-4">
+                  <span className="-rotate-12">
+                    <LuCupSoda size={50} />
+                  </span>
+                  <h1 className="text-2xl md:text-5xl lg:text-5xl blue-text-shadow">
+                    My Movies
+                  </h1>
+                  <span className="rotate-12">
+                    <PiPopcornBold size={50} />
+                  </span>
+                </div>
+                <section className="w-full grid g grid-cols-1 md:grid-cols-2 lg:grid-cols-4 p-10 gap-6 rounded-lg">
+                  {movies.map((movie) => (
+                    <section
+                      className="w-full rounded-lg border-pinkNeon shadow-lg shadow-pinkNeon border-2"
+                      key={movie.id}
+                    >
+                      <Link href="/movie/:id">
+                        <img
+                          className="w-full rounded-lg"
+                          src={movie.cover}
+                          alt="movie"
+                        />
+                      </Link>
+                      <div className="flex items-center justify-between py-3 border-t-2 border-pinkNeon px-2">
+                        <h1 className="text-[#FDC580] font-bold">
+                          {movie.title}
+                        </h1>
+                        <div className="flex-center gap-3">
+                          <span className="text-white">
+                            <RiDeleteBinLine size={22} />
+                          </span>
+                        </div>
                       </div>
-                    </div>
-                  </section>
-                ))}
-              </section>
-            </div>
+                    </section>
+                  ))}
+                </section>
+              </div>
+            )}
           </div>
         </div>
       </div>
