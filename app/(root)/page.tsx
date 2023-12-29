3551deac197ede71b0d57356eae0f11ef120499b
Igor <igormraesmm@gmail.com>
2023-12-29 11:31:55 -0300

    refactor: Trying to fix bug

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,9 @@
 import Vhs from "../components/vhs/page"
-import Home from "./home/page"
 
 export default async function PreHome() {
   return (
     <>
-      <Home />
+      <Vhs />
     </>
   )
 }
