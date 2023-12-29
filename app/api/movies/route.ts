2de0e5ca724cb4059ae548f407377a58dfe7047b
Igor <igormraesmm@gmail.com>
2023-12-29 11:20:53 -0300

    refactor: Remove unnecessary files

--- app/api/movies/route.ts
+++
@@ -1,8 +0,0 @@
-import prisma from "@/lib/db"
-import { NextResponse } from "next/server"
-
-export async function GET() {
-  const movies = await prisma.movie.findMany()
-
-  return NextResponse.json(movies)
-}
