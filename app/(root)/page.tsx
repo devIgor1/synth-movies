c1609afd64618219d3322310738537082fe8471f
Igor <igormraesmm@gmail.com>
2023-12-29 10:23:48 -0300

    refactor: Remove unnecessary files

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,15 +1,6 @@
-import { getServerSession } from "next-auth"
 import Vhs from "../components/vhs/Vhs"
-import { redirect } from "next/navigation"
-import { authOptions } from "@/lib/auth"
 
-export default async function Home() {
-  const session = await getServerSession(authOptions)
-
-  if (session) {
-    redirect("/home")
-  }
-
+export default async function PreHome() {
   return (
     <>
       <Vhs />
