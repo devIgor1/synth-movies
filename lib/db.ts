7057c3425df16d8b4e1cc6a0a67bdcaf213460c9
Igor <igormraesmm@gmail.com>
2023-12-29 11:41:21 -0300

    refactor: Apply another instantiating prismaClient

--- lib/db.ts
+++ lib/db.ts
@@ -1,19 +1,15 @@
 import { PrismaClient } from "@prisma/client"
 
-let prisma: PrismaClient
-
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient()
-} else {
-  let globalWithPrisma = global as typeof globalThis & {
-    prisma: PrismaClient
-  }
-
-  if (!globalWithPrisma.prisma) {
-    globalWithPrisma.prisma = new PrismaClient()
-  }
-
-  prisma = globalWithPrisma.prisma
+const prismaClientSingleton = () => {
+  return new PrismaClient()
 }
 
+declare global {
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>
+}
+
+const prisma = globalThis.prisma ?? prismaClientSingleton()
+
 export default prisma
+
+if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma
