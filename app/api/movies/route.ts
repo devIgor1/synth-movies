9e55c848287865359241adba71d7073bc8851ecf
Igor <igormraesmm@gmail.com>
2023-12-29 10:20:49 -0300

    feat: Remove unnecessary files

--- app/api/movies/route.ts
+++
@@ -1,8 +0,0 @@
-import prisma from "@/lib/db"
-import { NextRequest, NextResponse } from "next/server"
-
-export async function GET(request: NextRequest) {
-  const movies = await prisma.movie.findMany()
-
-  return NextResponse.json(movies)
-}
